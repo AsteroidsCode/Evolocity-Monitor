e9d4b5c46c943cc69fbc48bda52a042f021dcab6
Jonathan Voss <jvo16075@tacol.nz>
2019-12-18 16:22:29 +1300

    Removed unused javascript files

--- public/scripts/main.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register("./serviceWorker.js")
-    .then((reg) => {
-      console.log('Service worker registered.', reg);
-    });
-  });
-}
