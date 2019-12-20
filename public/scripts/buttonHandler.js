8dad49f7d073acbd363d827b1949868e1654e0c2
Jonathan Voss <jvo16075@tacol.nz>
2019-12-20 13:26:52 +1300

    Added interface for Spotify integration.

---
+++ public/scripts/buttonHandler.js
@@ -0,0 +1,8 @@
+function openMusicWindow() {
+    var musicWindow = document.getElementById("musicWindow");
+  if (musicWindow.style.display === "none") {
+    musicWindow.style.display = "block";
+  } else {
+    musicWindow.style.display = "none";
+  }
+}
--- public/scripts/spotifyHandler.js
+++ public/scripts/spotifyHandler.js
@@ -1,5 +1,5 @@
 window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = 'BQCHsbvx637B4X56rW2Xw6iNa6u_zThtOa2qaD_BVWmSAek1mOIJvuHwkyPlvYiTwG7CRV7XY3Sg1Glij9tYsjhzhvG-Xm5eOXSWJEpJ5852k1eHZDNChUCcKikmygykx7hYBYBerX4b1FrClLmhfWKVFcrvMh0jVxBe1NTpflmkD3hO1p8';
+    const token = 'BQC_Cva2c3s7Qqk02zl0cFofTrObWpr5_xJXbzc8aofFxINcwMgvQeFYuFwz83hB0LimoikPyflPOJJNScBy7bWJNzQVUdpskAANipAHvc2xkgGYvNRu1b93m4Mf-jw6_6MCvYwI2RgedPMMnU7QhYHSlqREATlWsNz2ZtSHnBBkM-XJjeg';
     const player = new Spotify.Player({
         name: 'Green Model B',
         getOAuthToken: cb => {
@@ -32,6 +32,27 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     // Playback status updates
     player.addListener('player_state_changed', state => {
         console.log(state);
+        player.getCurrentState().then(state => {
+            if (!state) {
+                console.error('User is not playing music through the Web Playback SDK');
+                return;
+            }
+
+            let {
+                current_track,
+                next_tracks: [next_track]
+            } = state.track_window;
+
+            console.log('Currently Playing', current_track);
+            console.log('Playing Next', next_track);
+
+            albumImage = current_track.album.images[0].url;
+            songText = current_track.name;
+            artistText = current_track.artists[0].name;
+            document.getElementById("AlbumImage").style.backgroundImage = "url(" + albumImage + ")";
+            document.getElementById("SongText").innerHTML = songText;
+            document.getElementById("ArtistText").innerHTML = artistText;
+        });
     });
 
     // Ready
@@ -51,18 +72,20 @@ window.onSpotifyWebPlaybackSDKReady = () => {
     // Connect to the player!
     player.connect();
 
-    player.getCurrentState().then(state => {
-        if (!state) {
-            console.error('User is not playing music through the Web Playback SDK');
-            return;
+    function tooglePlayButton() {
+        var playButton = document.getElementById("alt-play");
+        if (playButton.innerHTML === " play_arrow ") {
+            playButton.innerHTML = " pause ";
+    
+            player.togglePlay().then(() => {
+                console.log('Toggled playback!');
+            });
+        } else {
+            playButton.innerHTML = " play_arrow ";
+    
+            player.togglePlay().then(() => {
+                console.log('Toggled playback!');
+            });
         }
-
-        let {
-            current_track,
-            next_tracks: [next_track]
-        } = state.track_window;
-
-        console.log('Currently Playing', current_track);
-        console.log('Playing Next', next_track);
-    });
+    }
 };
