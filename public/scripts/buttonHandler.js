67f0c5bce3f99fb55d32bc6bd577695961cf00b6
Jono Voss <jvo16075@tacol.nz>
2020-01-10 13:39:32 +1300

    Added Settings Panel

--- public/scripts/buttonHandler.js
+++ public/scripts/buttonHandler.js
@@ -1,8 +1,21 @@
 function openMusicWindow() {
-    var musicWindow = document.getElementById("musicWindow");
+  var musicWindow = document.getElementById("musicWindow");
+  var settingsWindow = document.getElementById("settingWindow");
   if (musicWindow.style.display === "none") {
     musicWindow.style.display = "block";
+    settingsWindow.style.display = "none";
   } else {
     musicWindow.style.display = "none";
+  }
+}
+
+function openSettingsWindow() {
+  var musicWindow = document.getElementById("musicWindow");
+  var settingsWindow = document.getElementById("settingWindow");
+  if (settingsWindow.style.display === "none") {
+    settingsWindow.style.display = "block";
+    musicWindow.style.display = "none";
+  } else {
+    settingsWindow.style.display = "none";
   }
 }
