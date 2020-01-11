33495d9c5ff99d98bf27914f2c0d4d597098b7fb
Jono Voss <jvo16075@tacol.nz>
2020-01-11 13:02:02 +1300

    Added framework for settings window

--- public/scripts/buttonHandler.js
+++ public/scripts/buttonHandler.js
@@ -19,3 +19,45 @@ function openSettingsWindow() {
     settingsWindow.style.display = "none";
   }
 }
+
+function openContentEco() {
+  var spotifySet =  document.getElementById("SettingsSpotify");
+  var ecoSet = document.getElementById("SettingsEco");
+  var connectSet = document.getElementById("SettingsConnect");
+
+  if (ecoSet.style.display === "none") {
+    ecoSet.style.display = "block";
+    spotifySet.style.display = "none";
+    connectSet.style.display = "none";
+  } else {
+    ecoSet.style.display = "none";
+  }
+}
+
+function openContentConnect() {
+  var spotifySet =  document.getElementById("SettingsSpotify");
+  var ecoSet = document.getElementById("SettingsEco");
+  var connectSet = document.getElementById("SettingsConnect");
+
+  if (connectSet.style.display === "none") {
+    connectSet.style.display = "block";
+    ecoSet.style.display = "none";
+    spotifySet.style.display = "none";
+  } else {
+    connectSet.style.display = "none";
+  }
+}
+
+function openContentSpotify() {
+  var spotifySet =  document.getElementById("SettingsSpotify");
+  var ecoSet = document.getElementById("SettingsEco");
+  var connectSet = document.getElementById("SettingsConnect");
+
+  if (spotifySet.style.display === "none") {
+    spotifySet.style.display = "block";
+    ecoSet.style.display = "none";
+    connectSet.style.display = "none";
+  } else {
+    spotifySet.style.display = "none";
+  }
+}
